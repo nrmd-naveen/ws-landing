4b4f3faa88f811d70fc22f8eb798cacd255d712f
santhosh007s <santhoshperumal745sp@gmail.com>
2025-10-22 13:28:08 +0530

    conflict & first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import { Header } from "@/components/header"
-import { Hero } from "@/components/hero"
-import { HowItWorks } from "@/components/how-it-works"
-import { WhyChoose } from "@/components/why-choose"
-import { Testimonials } from "@/components/testimonials"
-import { Founder } from "@/components/founder"
-import { CTA } from "@/components/cta"
-import { FAQ } from "@/components/faq"
-import { FashionBrand } from "@/components/fashion-brand"
-import { Footer } from "@/components/footer"
+import { Header } from "@/components/header";
+import { Hero } from "@/components/hero";
+import { HowItWorks } from "@/components/how-it-works";
+import { WhyChoose } from "@/components/why-choose";
+import { Testimonials } from "@/components/testimonials";
+import { Founder } from "@/components/founder";
+import { CTA } from "@/components/cta";
+import { FAQ } from "@/components/faq";
+import { FashionBrand } from "@/components/fashion-brand";
+import { Footer } from "@/components/footer";
+import MyJourneyPage from "@/components/timeline";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black overflow-hidden">
       <Header />
       <Hero />
-      <HowItWorks />
+      {/* <HowItWorks /> */}
+      <MyJourneyPage />
       <WhyChoose />
+
       <Testimonials />
       <Founder />
       <CTA />
@@ -23,5 +26,5 @@ export default function Home() {
       <FashionBrand />
       <Footer />
     </main>
-  )
+  );
 }
